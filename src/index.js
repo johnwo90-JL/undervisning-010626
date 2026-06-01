ae24d78db15cf90e7b1636d8afadde6ca59a0771
johnwo90-JL <john-age@jobloop.no>
2026-06-01 13:14:01 +0200

    Add server entry-point

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 
-console.log("Hello, World!");
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+const HOST = "127.0.0.1"; // "localhost" === "127.0.0.1"
+
+
+// Setup - Plugins, middlewares, endepunkt/handler, sette opp lytting
+
+// Plugins
+
+// Middlewares
+
+// Endpoints/Handlers
+
+// Lytting (Listening)
+
+app.listen(PORT, HOST, (err) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    console.log(`Server listening at ${HOST}:${PORT}`);
+})
