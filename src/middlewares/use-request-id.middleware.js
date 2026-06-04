111dd502e5cd346c04291581ff2801abc4766eb4
johnwo90-JL <john-age@jobloop.no>
2026-06-04 14:26:32 +0200

    feat: add `useRequestId`-middleware skeleton.

---
+++ src/middlewares/use-request-id.middleware.js
@@ -0,0 +1,10 @@
+import { uuidv4 } from "../utils/uuid.util.js";
+
+/**
+ * A middleware that attaches a request-ID (UUIDv4) to an incoming request
+ * @param {import("express").Request} req Request
+ * @param {import("express").Response} res Response
+ */
+export function useRequestId(req, res, next) {
+    req.id = null; // TODO Add actual request ID
+}
