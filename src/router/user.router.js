3bc821777ef83c595226f61073d09491415248f5
johnwo90-JL <john-age@jobloop.no>
2026-06-08 14:07:38 +0200

    Feat: Add "POST /" and "GET /:id" endpoints to router.

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -4,6 +4,9 @@ import { userController } from "../controllers/user.controller.js";
 export const userRouter = Router();
 
 userRouter.get("/", userController["/"]);
+userRouter.post("/", userController["[POST]/"]); // TODO Oppgave
+userRouter.post("/:id", userController["/:id"]); // TODO Leave for now, revert to GET soon-ish
+
 userRouter.get("/active", userController["/active"]);
 // userRouter.post("/", userController["/create"]);
 
