b14cc728c4a2301be930400569aad3ae9f2d9ead
johnwo90-JL <john-age@jobloop.no>
2026-06-16 10:58:02 +0200

    Debug: Add smoke-tests *directly in the main entry point because I'm lazy* (Do not do this unless it's for a minimal smoke-test!)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
 
 import express, { json } from "express";
 
+// Config
+import { config } from "./config/index.js";
+
+// Database provider
+import { db } from "./providers/db.provider.js";
+
+// Models
+import { UserModel } from "./models/user.model.js";
+
 // Routers
 import { rootRouter } from "./router/root.router.js";
 import { userRouter } from "./router/user.router.js";
 import { useRequestId } from "./middlewares/use-request-id.middleware.js";
 import { authnRouter } from "./router/authN.router.js";
+import { Op } from "sequelize";
 
+// Sync Database
+await db.sync();
+UserModel.create({
+    email: "admin@foobar.com",
+    password: "password1234",
+    lastLogin: Date.now()
+});
+
+UserModel.create({
+    email: "user@foobar.com",
+    password: "password1234",
+    lastLogin: Date.now()+9000
+});
+
+(await UserModel.findAll({
+    where: {
+        lastLogin: {
+            [Op.lt]: Date.now()
+        }
+    }
+})).forEach(e => e.destroy()); 
 
 export const app = express();
 
 const PORT = 3000;
 const HOST = "0.0.0.0"; // "localhost" === "127.0.0.1"
-
 
 // Setup - Plugins, middlewares, endepunkt/handler, sette opp lytting
 
