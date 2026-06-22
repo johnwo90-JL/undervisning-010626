36e88d94a936bee59f193780761ee37cde2b7518
johnwo90-JL <john-age@jobloop.no>
2026-06-22 14:35:47 +0200

    Feat: Add CardSchema for card data validation using Zod

---
+++ src/schema/card.schema.js
@@ -0,0 +1,19 @@
+import z from "zod";
+
+export const CardSchema = z.object({
+    id: z.uuidv4().optional(),
+    idProduct: z.number(),
+    idCategory: z.number(),
+    avg: z.number().optional().nullable(),
+    low: z.number().optional().nullable(),
+    trend: z.number().optional().nullable(),
+    avg1: z.number().optional().nullable(),
+    avg7: z.number().optional().nullable(),
+    avg30: z.number().optional().nullable(),
+    "avg-foil": z.number().optional().nullable(),
+    "low-foil": z.number().optional().nullable(),
+    "trend-foil": z.number().optional().nullable(),
+    "avg1-foil": z.number().optional().nullable(),
+    "avg7-foil": z.number().optional().nullable(),
+    "avg30-foil": z.number().optional().nullable(),
+});
