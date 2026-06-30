3721e5c1c661ea60bc1c35e9286dbe6411edbb03
johnwo90-JL <john-age@jobloop.no>
2026-06-30 10:37:06 +0200

    feat: add default JWT_VALID_FOR configuration in env settings

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -5,4 +5,5 @@ config();
 export const env = {
     NODE_ENV: process.env.NODE_ENV,
     JWT_SECRET: process.env.JWT_SECRET || "dev-test-secret",
+    JWT_VALID_FOR: process.env.JWT_VALID_FOR || "30s",
 }
